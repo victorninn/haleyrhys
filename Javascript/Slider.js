01a3daa6dd208b688fd501db54f16132a891b9b6
victorninn <victorninn@gmail.com>
2024-03-28 19:39:52 +0800

    Add files via upload

---
+++ Javascript/Slider.js
@@ -0,0 +1,29 @@
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.container');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex++;
+    if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    }
+    showSlide(currentIndex);
+}
